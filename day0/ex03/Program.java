65370915e641be118389935a084ae8cc5d5134fb
sl16 <vojtabar119@gmail.com>
2025-07-02 15:27:05 +0200

    day0 progress

--- day0/ex03/Program.java
+++ day0/ex03/Program.java
@@ -4,32 +4,51 @@ import java.util.Scanner;
 
 public class Program {
 	private static int weekNum = 1;
-	private static String weeklyGrades;
+	private static String weeklyGrades = "";
+	public static final int MAX_WEEKS = 18;
 	private static Scanner scanner;
 
 	public static void main(String[] argv) {
 		scanner = new Scanner(System.in);
 
 		while (scanner.hasNext()) {
-			if (scanner.nextInt() == 42)
-				//end and calculate
-			parseWeek();
+			if (weekNum > MAX_WEEKS)
+				calculateAndExit();
+			if (!scanner.hasNextInt()) {
+				if (scanner.next().equals("Week"))
+					parseWeek();
+				else
+					exitError();
+			}			
 			parseGrades();
 		}
-
+		scanner.close();
 	}
 
 	private static void calculateAndExit() {
-		
+		Scanner gradeScanner = new Scanner(weeklyGrades);
+
+		int weeklyGrade;
+		int i = 1;
+		while (gradeScanner.hasNextInt()) {
+			weeklyGrade = gradeScanner.nextInt();
+			System.out.printf("Week %d: ", i++);
+			for (int j = 0; j < weeklyGrade; j++)
+				System.out.print("=");
+			System.out.print(">\n");
+		}
+		gradeScanner.close();
+		if (scanner != null)
+			scanner.close();
+		System.exit(0);
 	}
 
 	private static void parseWeek() {
-		if (scanner.next().equals("Week")) {
-			if (scanner.nextInt() == weekNum)
-				weekNum++;
-			else
-				exitError();
-		}
+		int tmp = scanner.nextInt();
+		if (tmp == 42)
+			calculateAndExit();
+		if (tmp != weekNum)
+			exitError();
 	}
 
 	private static void parseGrades() {
@@ -39,9 +58,14 @@ public class Program {
 			if (!scanner.hasNextInt())
 				exitError();
 			currentGrade = scanner.nextInt();
+			if (currentGrade == 42)
+				calculateAndExit();
+			if (currentGrade < 1 || currentGrade > 9)
+				exitError();
 			lowestGrade = currentGrade < lowestGrade ? currentGrade : lowestGrade;
 		}
 		weeklyGrades += lowestGrade + " ";
+		weekNum++;
 	}
 
 	private static void exitError() {
---
+++ day0/ex04/Program.java
@@ -0,0 +1,83 @@
+package day0.ex04;
+
+import java.util.Scanner;
+
+public class Program {
+	private static Scanner scanner;
+	private static char[] input;
+	private static int[] frequency;
+	private static int maxFrequency;
+	private static int[] topTenFrequency;
+	private static char[] topTenChars;
+	
+	public static void main(String[] arvg) {
+		scanner = new Scanner(System.in);
+		String str = scanner.next();
+		scanner.close();
+
+		if (str.length() > 999)
+			exitError();
+
+		input = str.toCharArray();
+		storeInArr();
+		findTopTen();
+
+		printResult();
+	}
+
+	private static void printResult() {
+		maxFrequency = topTenFrequency[0];
+		int scaledStep;
+		if (maxFrequency / 10 > 9)
+			scaledStep = maxFrequency / 10;
+		else
+			scaledStep = 1;
+
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (topTenFrequency[j] >= maxFrequency - (i * scaledStep))
+					System.out.printf("# ");
+				else
+					System.out.printf("  ");
+			}
+			System.out.printf("\n");
+		}
+		
+		for (int i = 0; i < 10; i++)
+			System.out.printf("%c ", topTenChars[i]);
+	}
+
+	private static void findTopTen() {
+		topTenFrequency = new int[10];
+		topTenChars = new char[10];
+
+		for (int i = 0; i < 10; i++) {
+			int highest = 0;
+			int index = -1;
+
+			for (int j = 0; j < frequency.length; j++) {
+				if (frequency[j] > highest)
+					highest = frequency[j];
+					index = j;
+			}
+
+			if (index != -1) {
+				topTenFrequency[i] = highest;
+				topTenChars[i] = (char) index;
+				frequency[index] = 0;
+			}
+		}
+	}
+
+	private static void storeInArr() {
+		frequency = new int[65536];
+		for (int i = 0; i < input.length; i++) {
+			frequency[input[i]]++;
+		}
+	}
+
+	private static void exitError() {
+		System.err.println("IllegalArgument");
+		System.exit(-1);
+	}
+}
